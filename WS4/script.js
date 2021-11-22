83d47e303796f5fe4a0da3c270edeb8ec6d0950b
Pierre <pierre.sjoberg@kau.se>
2021-11-22 14:11:51 +0100

    WS4 ex2

--- WS4/script.js
+++ WS4/script.js
@@ -1,10 +1,23 @@
 'use strict';
-
+/*
+//Lösningsförlsag uppgift 1
 document.writeln('<h1>Lottorad</h1>');
 
-for(let i=1; i<36; i++) {
-    document.write(i+' ');
-    if(i%5===0) {
+for(let i = 1; i <= 35 ; i++) {
+    document.write(i + ' ');
+    if(i%5==0) {
         document.write('<br>');
     }
 }
+*/
+
+document.writeln('<h1>Lottorad</h1>');
+document.writeln('<table>');
+document.writeln('<tr>');
+for (let i = 1; i <= 35; i++) {
+    document.write('<td>' + i + '</td>');
+    if (i % 5 == 0) {
+        document.write('</tr><tr>');
+    }
+}
+document.writeln('</tr></table>');
