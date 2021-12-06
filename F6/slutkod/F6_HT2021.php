6de27ddd4a467166a77b1649116f1e20888ec91f
peteratkau <peter.bellstrom@kau.se>
2021-12-06 12:18:22 +0100

    Uppdaterat F6 och W6

--- F6/slutkod/F6_HT2021.php
+++ F6/slutkod/F6_HT2021.php
@@ -8,13 +8,42 @@
         <main>
             <?php 
             
-                //Kodexempel med utskrift
+                //Kodexempel med utskrift och variabler
+                $kurskod = "ISGB02";
+                echo($kurskod);
 
-                //Kodexempel med variabler
+                $kursnamn = "Webbutveckling";
+                $hp = 7.5;
+
+                echo("<p>" . $kurskod . "</p>");
+                echo("<p>$kurskod, $kursnamn, $hp");
+
+                echo("<p>" . gettype($kurskod) . " " . gettype($hp) . "</p>");
+
+                echo("<script>alert('hahaha');</script>");
+
+                //För gettype() och settype() se 
+                //https://www.php.net/manual/en/function.gettype.php
+                //https://www.php.net/manual/en/function.settype.php
 
                 //Kodexempel med selektion
+                $klienthp = 3.5;
+                $serverhp = 4.0;
+                $helkurs = 7.5;
+                if($klienthp + $serverhp === $helkurs) {
+                    echo("<p>Grattis du har klarat hela kursen!</p>");
+                } else {
+                    echo("<p>Nja du har nog ngt kvar</p>");
+                }
 
                 //Kodexempel med iteration
+                for($i = 1; $i < 7;  $i++) {
+                    echo("<h$i>Detta är rubriknivå $i</h$i>"). PHP_EOL;
+                    echo("<script>alert('inte igen...');</script>");
+                }
+
+                echo("<div style='color: pink;'>Detta är en</div>");
+                echo("<div style=\"color: lightblue;\">Detta är en text!</div>");
               
             ?>
         </main>
---
+++ WS6/startkod/W6_HT2021.php
@@ -0,0 +1,51 @@
+local<!DOCTYPE html>
+<html lang="sv">
+    <head>
+        <meta charset="utf-8">
+        <title>PHP W6</title>
+
+        <!-- 9. -->
+        <style>
+            img { height: 20%; widht: 20%; }
+        </style>
+    </head>
+
+    <body>
+
+        <main>    
+            <?php
+
+                /*
+                    Bilder på tärningar (1-6):
+
+                    <img src="https://openclipart.org/download/282132/Die6.svg" />
+                    <img src="https://openclipart.org/download/282131/Die5.svg" />
+                    <img src="https://openclipart.org/download/282130/Die4.svg" />
+                    <img src="https://openclipart.org/download/282129/Die3.svg" />
+                    <img src="https://openclipart.org/download/282128/Die2.svg" />
+                    <img src="https://openclipart.org/download/282127/Die1.svg" />
+                    
+                    1. Slumpa ett tal mellan 1 och 6 och skriv ut resultatet.
+                        se rand() funktionen på https://www.php.net/manual/en/function.rand.php
+                    2. Skriv ut resultatet från punkt 1 i ett p-element.
+                    3. Med iteration (for) slumpa 6 tal mellan 1 och 6 och skriv ut resultatet.
+                    4. Skriv ut resultatet från punkt 3 i ett div-element och varje framslumpat tal i 
+                        ett eget p-element.
+                    5. Summera alla framslumpade tal och skriv ut summan i ett eget p-element.
+                    6. För varje framslupat tal mellan 1 och 6 skriv ut ett img-element som pekar
+                        (använd src-attributet) på en bild som matchar en tärning (se ovan).
+                    7. Om tid finns! Skapa använd instruktionerna ovan för img-element och skapa en vektor där varje img-element
+                        finns i ett eget index. Revidera sedan punkt 6 och använd vektorn och ett framslupat värde (=vektorindex)
+                        för att skriva ut img-element instruktionerna.
+                    8. Gör om punkt 3 och 4 med med while och do while iteration.
+                    9. Använd CSS för att minska bilderna till en storlek där alla tre tärningar visas på samma rad.
+                    10. Lägg till alt-attributet för bilderna och validera sedan er kod.
+                */
+
+            ?>
+               
+        </main>
+
+    </body>
+
+</html>
