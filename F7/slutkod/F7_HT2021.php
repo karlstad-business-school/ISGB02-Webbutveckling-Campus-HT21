dbbfcc46408e32643c01461a6f53d3bdab5b1652
peteratkau <peter.bellstrom@kau.se>
2021-12-07 13:19:35 +0100

    Uppdaterat F7

--- F7/slutkod/F7_HT2021.php
+++ F7/slutkod/F7_HT2021.php
@@ -11,6 +11,56 @@
             //Exempel för att skapa databasen i phpMyAdmin
                 
             //Kodexempel med databaskoppling och select utan villkor
+            $dns = "mysql:host=localhost;dbname=bil;charset=utf8";
+            $username = "root";
+            $password = "";
+
+            //Koppla upp mot DBHS mot given DB med användare och lösenord
+            $dbh = new PDO($dns, $username, $password);
+            
+            echo("<p>Japp det fungerar!</p>");
+            
+            //Skapa en SQL-sats och observera att den är utan villkor!
+            //$sql = "SELECT * FROM cars;";
+
+            $sql = "SELECT * FROM cars WHERE fabrikat = :fabrikat AND farg != :farg;";
+
+            $stmt = $dbh->prepare($sql);
+            
+            $stmt->bindValue(":fabrikat", "Kia");
+            $stmt->bindValue(":farg", "#000000");
+
+            $stmt->execute();
+
+            //Detta är bara för test och felsökning!
+            
+            //$table = $stmt->fetchAll();
+            /*
+            echo("<pre>");
+            print_r($table);
+            echo("</pre>");
+            */
+
+            /*
+            foreach($table as $row) {
+                echo($row["id"] . " " . $row["fabrikat"]);
+            }
+            */
+
+            echo("<pre>");
+            while($row = $stmt->fetch()) {
+                //print_r($row);
+                //echo($row[0] . " " . $row["mil"]);
+
+                echo("<p>id: " . $row["id"] . "</p>");
+            }
+            echo("</pre>");
+
+
+
+            $stmt = null;
+            //Stäng ner kopplingen mot DBHS...
+            $dbh = null;
 
             //Kodexempel med databaskoppling och select med villkor
 
