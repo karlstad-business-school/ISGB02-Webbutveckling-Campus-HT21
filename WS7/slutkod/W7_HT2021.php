6170d1e7c3cae6ade7e00a6645584f1f722a52f7
peteratkau <peter.bellstrom@kau.se>
2021-12-07 15:28:12 +0100

    Uppdaterat W7

--- WS7/slutkod/W7_HT2021.php
+++ WS7/slutkod/W7_HT2021.php
@@ -21,6 +21,55 @@
                 //6. På valfritt sätt lista innehållet i tabellen cars.
                 //7. Frigör minnet och stäng ner databaskopplingen.
 
+                $dns = "mysql:host=localhost;dbname=car;charset=utf8";
+                $userName = "root";
+                $password = "";
+    
+                $dbh = new PDO($dns, $userName, $password);
+
+                $fabrikat = "Volvo";
+                $modell = "XC90";
+                $regnr = "MMM890";
+                $farg = "#0000ff";
+    
+                //Skapa frågan
+                $sql = "INSERT INTO cars(fabrikat, modell, regnr, farg) VALUES(:fabrikat, :modell, :regnr, :farg);";
+    
+                //Preparera frågan
+                $stmt = $dbh->prepare($sql);
+                $stmt->bindValue(":fabrikat", $fabrikat);
+                $stmt->bindValue(":modell", $modell);
+                $stmt->bindValue(":regnr", $regnr);
+                $stmt->bindValue(":farg", $farg);
+
+                //Exekvera frågan mot db
+                $stmt->execute();
+
+                //Lista på valfritt sätt
+                $sql = "SELECT * FROM cars;";
+                $stmt = $dbh->prepare($sql);
+                $stmt->execute();
+    
+                //Visa data som vi fick i retur från databasen
+                echo("<h2>Alla bilar i databasen!</h2>");
+    
+                while( $row = $stmt->fetch() ) {
+    
+                    $id = $row["id"];
+                    $fabrikat = $row["fabrikat"];
+                    $modell = $row["modell"];
+                    $regnr = $row["regnr"];
+                    $farg = $row["farg"];
+    
+                    echo("<p style=\"background: $farg\">id: $id, fabrikat: $fabrikat, modell: $modell, regnr: $regnr</p>") . PHP_EOL;
+    
+                }
+    
+                //Frigör minnet för returen från frågan och stäng ner kopplingen
+                $stmt = null;
+                $dbh = null;
+
+
                 //B
                 //1. Koppla upp PHP-applikationen med databasen car. Ange de värden som behövs.
                 //2. Skapa variablerna fabrikat, modell, regnr och farg och tilldela dessa testdata.
@@ -31,6 +80,58 @@
                 //7. På valfritt sätt lista innehållet i tabellen cars.
                 //8. Frigör minnet och stäng ner databaskopplingen.
 
+                
+                $dns = "mysql:host=localhost;dbname=car;charset=utf8";
+                $userName = "root";
+                $password = "";
+    
+                $dbh = new PDO($dns, $userName, $password);
+
+                $fabrikat = "Saab";
+                $modell = "900";
+                $regnr = "MMM890";
+                $farg = "#00ff00";
+                $id = 32;
+    
+                //Skapa frågan
+                $sql = "UPDATE cars SET fabrikat = :fabrikat, modell = :modell, regnr = :regnr, farg = :farg";
+                $sql .= " WHERE id = :id;";
+    
+                //Preparera frågan
+                $stmt = $dbh->prepare($sql);
+                $stmt->bindValue(":fabrikat", $fabrikat);
+                $stmt->bindValue(":modell", $modell);
+                $stmt->bindValue(":regnr", $regnr);
+                $stmt->bindValue(":farg", $farg);
+                $stmt->bindValue(":id", $id);
+
+                //Exekvera frågan mot db
+                $stmt->execute();
+
+                //Lista på valfritt sätt
+                $sql = "SELECT * FROM cars;";
+                $stmt = $dbh->prepare($sql);
+                $stmt->execute();
+    
+                //Visa data som vi fick i retur från databasen
+                echo("<h2>Alla bilar i databasen!</h2>");
+    
+                while( $row = $stmt->fetch() ) {
+    
+                    $id = $row["id"];
+                    $fabrikat = $row["fabrikat"];
+                    $modell = $row["modell"];
+                    $regnr = $row["regnr"];
+                    $farg = $row["farg"];
+    
+                    echo("<p style=\"background: $farg\">id: $id, fabrikat: $fabrikat, modell: $modell, regnr: $regnr</p>") . PHP_EOL;
+    
+                }
+    
+                //Frigör minnet för returen från frågan och stäng ner kopplingen
+                $stmt = null;
+                $dbh = null;
+
                 //C
                 //1. Koppla upp PHP-applikationen med databasen car. Ange de värden som behövs.
                 //2. Skapa varibeln id och tilldela den ett värde som finns i databasen (primärnyckel för cars).
@@ -39,6 +140,48 @@
                 //5. Exekvera frågan.
                 //6. På valfritt sätt lista innehållet i tabellen cars.
                 //7. Frigör minnet och stäng ner databaskopplingen.
+
+                 
+                $dns = "mysql:host=localhost;dbname=car;charset=utf8";
+                $userName = "root";
+                $password = "";
+    
+                $dbh = new PDO($dns, $userName, $password);
+                $id = 32;
+    
+                //Skapa frågan
+                $sql = "DELETE FROM cars WHERE id = :id;";
+    
+                //Preparera frågan
+                $stmt = $dbh->prepare($sql);
+                $stmt->bindValue(":id", $id);
+
+                //Exekvera frågan mot db
+                $stmt->execute();
+
+                //Lista på valfritt sätt
+                $sql = "SELECT * FROM cars;";
+                $stmt = $dbh->prepare($sql);
+                $stmt->execute();
+    
+                //Visa data som vi fick i retur från databasen
+                echo("<h2>Alla bilar i databasen!</h2>");
+    
+                while( $row = $stmt->fetch() ) {
+    
+                    $id = $row["id"];
+                    $fabrikat = $row["fabrikat"];
+                    $modell = $row["modell"];
+                    $regnr = $row["regnr"];
+                    $farg = $row["farg"];
+    
+                    echo("<p style=\"background: $farg\">id: $id, fabrikat: $fabrikat, modell: $modell, regnr: $regnr</p>") . PHP_EOL;
+    
+                }
+    
+                //Frigör minnet för returen från frågan och stäng ner kopplingen
+                $stmt = null;
+                $dbh = null;
 
             ?>
         </main>
