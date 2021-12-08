debcf55e0ba45342474ded64eb6c82ee832f8b68
Pierre <pierre.sjoberg@kau.se>
2021-12-08 11:08:16 +0100

    F8 halvvägs

--- F8/F8_HT2021.php
+++ F8/F8_HT2021.php
@@ -1,3 +1,25 @@
+<?php
+
+class bil {
+
+    private $fabrikat;
+    private $modell;
+    private $regnr;
+    private $farg;
+
+    public function __construct($infabrikat,$inmodell,$inregnr,$infarg) {
+
+        $this->$fabrikat=$infabrikat;
+        $this->$modell=$inmodell;
+        $this->$regnr=$inregnr;
+        $this->$farg=$infarg;
+    }
+
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="sv">
     <head>
@@ -7,7 +29,31 @@
     <body>
         <main>
             <?php 
-                
+                    $dns="mysql:host=localhost;dbname=bil;charset=utf8";
+    $userName="root";
+    $password="";
+
+    $dbhOptions=array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION );
+
+    $dbh = new PDO($dns,$userName, $password, $dbhOptions);
+
+    $sql = "SELECT * FROM cars";
+
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach($data as $row) {
+
+        foreach($row as $col) {
+            echo($col . "<br>"); 
+        }
+        echo("<hr>");
+    }
+
+    $stmt = null;
+    $dbh=null;
            
             ?>
 			<!--
