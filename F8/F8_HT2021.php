d849a8a2d2d444c5771e7bf26aa7fa8ec45611c6
Pierre <pierre.sjoberg@kau.se>
2021-12-08 10:28:02 +0100

    startkod F8

---
+++ F8/F8_HT2021.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="sv">
+    <head>
+        <meta charset="utf-8">
+        <title>PHP F8</title>
+    </head>
+    <body>
+        <main>
+            <?php 
+                
+           
+            ?>
+			<!--
+            <form action="F8_HT2021.php" method="post">
+                <input type="text" name="fabrikat" />
+                <input type="text" name="modell" />
+                <input type="text" name="regnr" />
+                <input type="color" name="farg" />
+                <input type="submit" name="btnSend" value="Skicka" />
+            </form>
+			-->
+        </main>
+    </body>
+</html>
